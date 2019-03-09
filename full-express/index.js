864de67816b518e272ddfd0db64328b0fe476829
Mario Nemuel Flores Condori <marioflorescondori@gmail.com>
2019-03-09 17:00:33 -0400

    practicing with express adding basic features

--- full-express/index.js
+++ full-express/index.js
@@ -1,14 +1,59 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'pug');
+app.set('views',path.join(__dirname, 'views'));
 
-app.get('/',function (req,res){
-    res.send('hello world');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'img')));
+
+app.use(function(req, res, next) {
+	console.log("start");
+	next();
+});
+
+app.get('/',function (req,res,next){
+    res.send('getting message');
+    next();
 /*     console.log("sending page...");
     res.end('another message'); */
 });
 
+app.get('/first-template',function(req, res,next){
+	//res.send("temlate!");
+	//console.log("first_view");
+	res.render('first_view',{name:"tutorial full", home_url:"/"});
+	next();
+});
+
+
+
+
+app.post('/',(req, res)=>{
+    console.log("sending a post");
+    console.log(req.body);
+    console.log(`name = ${req.body.name}`);
+    res.send("this is post");
+});
+
+app.use(function(req, res, next) {
+	console.log("end");
+	next();
+});
+
+app.use(function(req, res) {
+	console.log("end2");
+});
+
+
 app.listen(app.get('port'), () => {
-    console.log(`server on port ${app.get('port')}`)
+    console.log(`server on port ${app.get('port')}`);
 });
